1e74f7bf4041d69ad5fb9c5e71a985177e533557
Secure-UPMEM <150752099+Secure-UPMEM@users.noreply.github.com>
2024-04-14 14:57:35 -0700

    Delete MLP directory

--- MLP/run.sh
+++
@@ -1,6 +0,0 @@
-NR_DPUS=2050lks  NR_TASKLETS=16 make -B
-# ./bin/gemv_host -m 1000 -n 1000 -e 10 
-# ./bin/gemv_host -m 5000 -n 5000 -e 10 -b 64
-./bin/gemv_host -m 10000 -n 10000 -e 10 -b 64
-# NR_DPUS=2  NR_TASKLETS=2 make -B
-# ./bin/gemv_host -m 4 -n 4 -e 1 
